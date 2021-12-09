0a587a26532f6f88832bde72f748858177b9ce5d
victoriaguille <91439219+victoriaguille@users.noreply.github.com>
2021-12-09 18:51:20 -0500

    Update app.py
    edited beginning of flask app to use hawaii sqlite database

--- app.py
+++ app.py
@@ -1,7 +1,28 @@
 
 
 #import the dependency
-from flask import Flask
+from flask import Flask, jsonify 
+import datetime as dt
+import numpy as np 
+import pandas as pd 
+import sqlalchemy 
+from sqlalchemy.ext.automap import automap_base 
+from sqlalchemy.orm import Session 
+from sqlalchemy import create_engine, func 
+
+#set up database engine
+engine = create_engine("sqlite:///hawaii.sqlite")
+
+#reflect the db
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+
+#save as references
+Measurement = Base.classes.measurement 
+Station = Base.classes.station 
+
+#create a session link
+session = Session(engine)
 
 #create a new flask app instance
 app = Flask(__name__)
