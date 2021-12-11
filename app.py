d0384ccc8cec48a6f7b9d7b77dde27f8a09947c6
victoriaguille <91439219+victoriaguille@users.noreply.github.com>
2021-12-10 20:17:06 -0500

    Update app.py
    created third route in flask app

--- app.py
+++ app.py
@@ -47,6 +47,13 @@ def precipitation():
         filter(Measurement.date >= prev_year).all()
     precip = {date: prcp for date, prcp in precipitation}
     return jsonify(precip)
+
+#create third route
+@app.route("/api/v1.0/stations")
+def stations():
+    results = session.query(Station.station).all()
+    stations = list(np.ravel(results))
+    return jsonify(stations=stations)
     
 # 6. Define main behavior
 if __name__ == "__main__":
