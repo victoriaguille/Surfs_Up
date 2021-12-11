0c65f3959b84a7a7b4e82c1bbe846c55bd8e2114
victoriaguille <91439219+victoriaguille@users.noreply.github.com>
2021-12-10 20:37:02 -0500

    Update app.py
    created final routes

--- app.py
+++ app.py
@@ -65,6 +65,24 @@ def temp_monthly():
     temps = list(np.ravel(results))
     return jsonify(temps=temps)
 
+#create final routes
+@app.route("/api/v1.0/temp/<start>")
+@app.route("/api/v1.0/temp/<start>/<end>")
+def stats(start=None, end=None):
+    sel = [func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)]
+
+    if not end:
+        results = session.query(*sel).\
+            filter(Measurement.date >= start).all()
+        temps = list(np.ravel(results))
+        return jsonify(temps)
+
+    results = session.query(*sel).\
+        filter(Measurement.date >= start).\
+        filter(Measurement.date <= end).all()
+    temps = list(np.ravel(results))
+    return jsonify(temps) 
+    
 # 6. Define main behavior
 if __name__ == "__main__":
     app.run(debug=True)
